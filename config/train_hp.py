8b96d299cea1e3923983294e2ce91bd8ab582ff5
Riccardo <rtrocca@gmail.com>
2024-06-10 07:57:51 +0200

    Fix paths and add HP with TikToken encoder

--- config/train_hp.py
+++ config/train_hp.py
@@ -1,7 +1,7 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-hp-char'
+out_dir = 'out-hp'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10 # don't print too too often
@@ -10,10 +10,10 @@ log_interval = 10 # don't print too too often
 always_save_checkpoint = False
 
 wandb_log = False # override via command line if you like
-wandb_project = 'hp-char'
+wandb_project = 'hp'
 wandb_run_name = 'hp-mini-gpt'
 
-dataset = 'hp_char'
+dataset = 'hp'
 gradient_accumulation_steps = 1
 batch_size = 64
 block_size = 256 # context of up to 256 previous characters
